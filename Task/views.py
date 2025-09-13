5fafe70c71009aa5b3a332cfd0564db93a5b81a2
Franco <carabajalfranco100@gmail.com>
2025-09-13 04:01:46 -0300

    practica

---
+++ Task/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, 'signup.html',{
+        "form":UserCreationForm
+    })
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                #register User
+                user = User.objects.create_user(username=request.POST["unsername"], password=request.POST["password1"])
+                user.save()
+                return HttpResponse("Usuario Creado")
+            except:
+                return HttpResponse("Usuario ya existe")
+        return HttpResponse("Contraseñas No coinciden")
+    
